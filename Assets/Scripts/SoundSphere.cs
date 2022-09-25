4a0396bbe59b5d4bd118634093afbd16defca773
Alan-mag <alanmagelssen@gmail.com>
2022-09-24 19:42:03 -0700

    Update tree and orb assets

---
+++ Assets/Scripts/SoundSphere.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSphere : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.Rotate(0, 1 * Time.deltaTime, 0, Space.Self);
+    }
+}
